9b9c449066bde07b50fda9958a69dfb1b4e51782
fritziyeke12 <fritziyeke@gmail.com>
2023-02-21 14:07:58 +0000

    finito

--- Band.js
+++ Band.js
@@ -1,4 +1,4 @@
-const db = require('./db');
+const { db } = require('./db');
 const { DataTypes } = require("sequelize");
 
 // TODO - define the Band model
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
+const db = require("./db.js");
+
+
 
 module.exports = {
     Band,
